7c5be63e664b1893e374c2d9695151ae6a9e78e3
n3kosempai <alvarom12098@gmail.com>
2021-11-01 17:56:15 -0400

    add module with function for frequency

---
+++ modules/frequency.py
@@ -0,0 +1,12 @@
+
+def allfrequency(buffer: str):
+    """function for frequency of character."""
+
+    #count total space
+    count_space = buffer.count(' ')
+
+    #count letters and frequency
+
+    return count_space
+
+
